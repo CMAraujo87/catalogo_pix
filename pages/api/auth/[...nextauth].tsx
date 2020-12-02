db3f721e82c362e72b688c51b0804593fe95132e
Cleber Machado de Araujo <AraujoMachadoC@Gmail.com>
2020-12-02 04:24:15 +0000

    added Auth0 provider, parametrized credentials provider

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -2,33 +2,38 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
 const providers = [
-    Providers.Credentials({
-        // The name to display on the sign in form (e.g. 'Sign in with...')
-        name: 'Credentials',
-        // The credentials is used to generate a suitable form on the sign in page.
-        // You can specify whatever fields you are expecting to be submitted.
-        // e.g. domain, username, password, 2FA token, etc.
-        credentials: {
-            username: { label: "Username", type: "text", placeholder: "jsmith" },
-            password: { label: "Password", type: "password" }
-        },
-        authorize: async (credentials) => {
-            // Add logic here to look up the user from the credentials supplied
-            const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-
-            if (user) {
-                // Any object returned will be saved in `user` property of the JWT
-                return Promise.resolve(user)
-            } else {
-                // If you return null or false then the credentials will be rejected
-                return Promise.resolve(null)
-                // You can also Reject this callback with an Error or with a URL:
-                // return Promise.reject(new Error('error message')) // Redirect to error page
-                // return Promise.reject('/path/to/redirect')        // Redirect to a URL
-            }
-        }
-    })
 ];
+
+if (GetEnvBool('CREDENTIALS_ENABLE')) {
+    providers.push(
+        Providers.Credentials({
+            // The name to display on the sign in form (e.g. 'Sign in with...')
+            name: 'Credentials',
+            // The credentials is used to generate a suitable form on the sign in page.
+            // You can specify whatever fields you are expecting to be submitted.
+            // e.g. domain, username, password, 2FA token, etc.
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            authorize: async (credentials) => {
+                // Add logic here to look up the user from the credentials supplied
+                const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+
+                if (user) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return Promise.resolve(user)
+                } else {
+                    // If you return null or false then the credentials will be rejected
+                    return Promise.resolve(null)
+                    // You can also Reject this callback with an Error or with a URL:
+                    // return Promise.reject(new Error('error message')) // Redirect to error page
+                    // return Promise.reject('/path/to/redirect')        // Redirect to a URL
+                }
+            }
+        })
+    )
+}
 
 if (GetEnvBool('GITHUB_ENABLE')) {
     providers.push(
@@ -67,6 +72,16 @@ if (GetEnvBool('GOOGLE_ENABLE')) {
         Providers.Google({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET
+        })
+    )
+}
+
+if (GetEnvBool('AUTH0_ENABLE')) {
+    providers.push(
+        Providers.Auth0({
+            domain: process.env.AUTH0_DOMAIN,
+            clientId: process.env.AUTH0_ID,
+            clientSecret: process.env.AUTH0_SECRET
         })
     )
 }
